2ce82a3dab1cf135d795fc97280bd48bf67f25a2
Bernd Beuster <bernd.beuster@gmail.com>
2017-03-19 15:19:49 +0100

    Make (primep) faster
    Use (6k - 1) and (6k + 1) istead of (2k +1 ) as possible primes.
    
    See also: https://primes.utm.edu/notes/faq/six.html
              https://www.quora.com/Is-every-prime-number-other-than-2-and-3-of-the-form-6k±1

--- math.lisp
+++ math.lisp
@@ -36,12 +36,15 @@
 		     finally (return r))))
     (= n reversed)))
 
+;;; Use (6k - 1) and (6k + 1) as possible prime numbers.
 (defun primep (n)
   "Is N prime?"
-  (and (> n 1)
-       (or (= n 2) (oddp n))
-       (loop for i from 3 to (isqrt n) by 2
-	  never (zerop (rem n i)))))
+  (or (or (= n 2) (= n 3) (= n 5) (= n 7))
+      (and (> n 8)
+           (let ((r (rem n 6))) (or (= r 1) (= r 5)))
+           (loop for i from 6 to (max 6 (isqrt n)) by 6
+               never (or (zerop (rem n (1- i)))
+                         (zerop (rem n (1+ i))))))))
 
 ;;; http://groups.google.com.ai/group/comp.lang.lisp/msg/6bb75f006221fd4c
 (defun permutations (items)
